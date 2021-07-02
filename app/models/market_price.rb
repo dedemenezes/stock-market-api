a13afa1608fe0e71473668dd9980da564e25e358
Andre Menezes <djdedemenezes@gmail.com>
2021-07-02 11:56:17 -0300

    fixed validations

--- app/models/market_price.rb
+++ app/models/market_price.rb
@@ -1,6 +1,7 @@
 class MarketPrice < ApplicationRecord
   has_many :stocks
   
-  validates :price, uniqueness: { scope: :currency,
+  validates :value_cents, uniqueness: { scope: :currency,
     message: "Price already exists on this currency" }
+  validates :currency, :value_cents, presence: true
 end
