b377592e88c6b1b28d604fe4ea41d3be077f13a3
Andre Menezes <djdedemenezes@gmail.com>
2021-07-07 22:05:09 -0300

    created route for show controller and action for show

---
+++ app/controllers/api/v1/stocks_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::StocksController < Api::V1::BaseController
+  before_action :set_stock, only: :show
+  
+  def show
+  end
+  
+  private
+  
+  def set_stock
+    @stock = Stock.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :bearers, only: [:create, :show]
       resources :market_prices, only: [ :show, :create ]
+      resources :stocks, only: [ :show ]
     end
   end
   
