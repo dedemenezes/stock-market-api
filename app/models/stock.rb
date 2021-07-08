3394b55760ac23be9c6a708f2b26789cab7edd9e
Andre Menezes <djdedemenezes@gmail.com>
2021-07-07 21:08:58 -0300

    Fixed relartion

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
-  belongs_to :bearers
-  belongs_to :market_prices
+  belongs_to :bearer
+  belongs_to :market_price
 
   validates :name, :bearer, :market_price, presence: true
 end
