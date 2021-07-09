130d422c8c097176abdd4f80b555c78357ab99f8
Andre Menezes <djdedemenezes@gmail.com>
2021-07-09 11:25:40 -0300

    first job done

--- app/jobs/first_job.rb
+++ app/jobs/first_job.rb
@@ -1,8 +1,12 @@
 class FirstJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(user)
     puts "Starting my job!"
+    puts "zo/"
+    puts "zo_"
+    puts "zo/"
+    puts user.admin?
     sleep(2)
     puts "bye from your worker"
     # Do something later
