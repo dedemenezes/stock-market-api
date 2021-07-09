a1f965c7c58858550d0afaa1aa6401077359b3bf
Andre Menezes <djdedemenezes@gmail.com>
2021-07-09 11:03:53 -0300

    added sidekiq dashboard route for user admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  require "sidekiq/web"
+  authenticate :user -> (user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :bearers, only: [:create, :show]
